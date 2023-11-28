6c7fc0f80c7055a35ef609bf6e411a5838a98d20
Danilo <dansantosalfa@gmail.com>
2023-11-28 17:30:09 -0300

    feat: Class Mercado

---
+++ Mercado.rb
@@ -0,0 +1,10 @@
+class Mercado
+  def initialize(product, price)
+    @product = product
+    @price = price
+  end
+
+  def buy
+        puts "Você comprou o #{product} pelo valor de R$#{price}"
+  end
+end
